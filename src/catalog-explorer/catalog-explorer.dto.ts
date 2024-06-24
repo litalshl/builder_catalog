4acd13eb0353e252b8feb1aa9a2bc86c0e06e8ae
litalshl <lital.shalev@gmail.com>
2024-06-24 21:26:23 +0300

    Added unit tests and some dummy implementation

---
+++ src/catalog-explorer/catalog-explorer.dto.ts
@@ -0,0 +1,31 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class Brick {
+  @Field()
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field(() => Int)
+  width: number;
+
+  @Field(() => Int)
+  length: number;
+
+  @Field()
+  colorId: string;
+}
+
+@ObjectType()
+export class Set {
+  @Field()
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field(() => [Brick])
+  bricks: Brick[];
+}
