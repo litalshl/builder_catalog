92bd59b9a56e88bc0455c4ebb780d9c1510350bf
litalshl <lital.shalev@gmail.com>
2024-06-25 11:35:54 +0300

    Update user inventory transformation and tests mock data

--- src/catalog-explorer/catalog-explorer.service.ts
+++ src/catalog-explorer/catalog-explorer.service.ts
@@ -18,12 +18,14 @@ export class CatalogExplorerService {
   }
 
   async getSets() {
-    const response = await lastValueFrom(this.httpService.get(`${this.apiUrl}/sets`).pipe(map(response => response.data)));
+    const response = await lastValueFrom(this.httpService.get(`${this.apiUrl}/sets`).pipe(
+      map(response => response.data)));
     return response;
   }
 
   async getSetById(id: string) {
-    const response = await lastValueFrom(this.httpService.get(`${this.apiUrl}/set/by-id/${id}`).pipe(map(response => response.data)));
+    const response = await lastValueFrom(this.httpService.get(`${this.apiUrl}/set/by-id/${id}`).pipe(
+      map(response => response.data)));
     return response;
   }
 
@@ -31,7 +33,7 @@ export class CatalogExplorerService {
     const user = await this.getUserByUsername(username);
     const sets = await this.getSets();
 
-    const userInventory = this.transformInventory(user.inventory);
+    const userInventory = this.transformUserInventory(user.collection);
 
     const buildableSets = [];
     for (const set of sets) {
@@ -43,17 +45,19 @@ export class CatalogExplorerService {
     return buildableSets;
   }
 
-  private transformInventory(inventory) {
+  private transformUserInventory(collection) {
     const transformed = {};
-    inventory.forEach(item => {
-      const key = `${item.brick.id}_${item.brick.colorId}`;
-      transformed[key] = item.count;
+    collection.forEach(item => {
+      item.variants.forEach(variant => {
+        const key = `${item.pieceId}_${variant.color}`;
+        transformed[key] = variant.count;
+      })
     });
     return transformed;
   }
 
   private canBuildSet(set: Set, userInventory): boolean {
-    const setInventory = this.transformInventory(set.bricks);
+    const setInventory = this.transformUserInventory(set.bricks);
     for (const key in setInventory) {
       if (!userInventory[key] || userInventory[key] < setInventory[key]) {
         return false;
